cfbc9d14ee219bfa632dea991cd1e2c67796d598
Serhii Yehorov <kicumzp@gmail.com>
2023-03-02 17:54:24 +0200

    first commit lesson 21

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let numberOne = 0;
+let numberTwo = 0;
+
+numberOne = prompt(`Input first number`); 
+numberTwo = prompt(`Input second number`); 
+alert(`Addition result is : ${+numberOne + +numberTwo}!`);
+alert(`Subtraction  result is : ${Number(numberOne) - Number(numberTwo)}!`);
+alert(`Multiplication  result is : ${+(numberOne) * +(numberTwo)}!`);
+alert(`Division result is : ${parseInt(numberOne) / parseInt(numberTwo)}!`);
