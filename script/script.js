b869d3f1d95137168fafa9602a707db7b514abc0
Serhii Yehorov <kicumzp@gmail.com>
2023-03-02 21:21:58 +0200

    new branch

--- script/script.js
+++ script/script.js
@@ -3,7 +3,35 @@ let numberTwo = 0;
 
 numberOne = prompt(`Input first number`); 
 numberTwo = prompt(`Input second number`); 
-alert(`Addition result is : ${+numberOne + +numberTwo}!`);
-alert(`Subtraction  result is : ${Number(numberOne) - Number(numberTwo)}!`);
-alert(`Multiplication  result is : ${+(numberOne) * +(numberTwo)}!`);
-alert(`Division result is : ${parseInt(numberOne) / parseInt(numberTwo)}!`);
+
+if (numberOne === null || numberOne === '' || numberTwo == null || numberTwo === '') {
+    alert ("Ooops we got a trouble")
+
+}else if (numberTwo === '0') {
+    alert(`Addition result is : ${+numberOne + +numberTwo}!`);
+    alert(`Subtraction  result is : ${Number(numberOne) - Number(numberTwo)}!`);
+    alert(`Multiplication  result is : ${+(numberOne) * +(numberTwo)}!`);
+    alert(`Division result is a mistake!`);
+
+}else if (numberOne < numberTwo) {
+    if (confirm('Are you sure?') === true){
+        alert(`Addition result is : ${+numberOne + +numberTwo}!`);
+        alert(`Subtraction  result is : ${+(numberOne) - +(numberTwo)}!`);
+        alert(`Multiplication  result is : ${+(numberOne) * +(numberTwo)}!`);
+        alert(`Division result is : ${+(numberOne) / +(numberTwo)}!`);
+
+    } else {
+        alert(`Addition result is : ${+numberOne + +numberTwo}!`);
+        alert(`Multiplication  result is : ${+(numberOne) * +(numberTwo)}!`);
+        alert(`Division result is : ${+(numberOne) / +(numberTwo)}!`);
+    }
+    
+     
+} else {
+    alert(`Addition result is : ${+numberOne + +numberTwo}!`);
+    alert(`Subtraction  result is : ${Number(numberOne) - Number(numberTwo)}!`);
+    alert(`Multiplication  result is : ${+(numberOne) * +(numberTwo)}!`);
+    alert(`Division result is : ${parseInt(numberOne) / parseInt(numberTwo)}!`);
+}
+
+
