1da7c17c219c1c8e104f2e69f07ca48e094222ee
Serhii Yehorov <kicumzp@gmail.com>
2023-03-21 14:12:38 +0200

    code refactoring

--- script/script.js
+++ script/script.js
@@ -1,24 +1,24 @@
-// let numberOne = 0;
-// let numberTwo = 0;
+let numberOne = 0;
+let numberTwo = 0;
 
-// numberOne = prompt(`Input first number`); 
-// numberTwo = prompt(`Input second number`); 
+numberOne = prompt(`Input first number`); 
+numberTwo = prompt(`Input second number`); 
 
-// if (!numberOne || !numberTwo ) {
-//     alert ("Ooops we got a trouble!")
-// }  else {
-//     numberOne = Number(numberOne)
-//     numberTwo = Number(numberTwo)
-//     let answer = true
+if (!numberOne || !numberTwo ) {
+    alert ("Ooops we got a trouble!")
+}  else {
+    numberOne = Number(numberOne)
+    numberTwo = Number(numberTwo)
+    let answer = true
 
-//     let divFunc = numberTwo ? (numberOne / numberTwo) : 'a MISTAKE!!!!'
+    let divFunc = numberTwo ? (numberOne / numberTwo) : 'a MISTAKE!!!!'
     
-//     if (numberOne < numberTwo) answer = confirm('Are you sure?')
+    if (numberOne < numberTwo) answer = confirm('Are you sure?')
 
-//     alert(`Addition result is  ${numberOne + numberTwo}!`);
-//     if(answer) alert(`Subtraction  result is  ${numberOne - numberTwo}!`);
-//     alert(`Multiplication  result is  ${numberOne * numberTwo}!`);
-//     alert(`Division result is  ${divFunc}`);
-// }
+    alert(`Addition result is  ${numberOne + numberTwo}!`);
+    if(answer) alert(`Subtraction  result is  ${numberOne - numberTwo}!`);
+    alert(`Multiplication  result is  ${numberOne * numberTwo}!`);
+    alert(`Division result is  ${divFunc}`);
+}
 
 
